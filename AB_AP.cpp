db1f3f49d190a2d6c0c660a9f40e54f0befbf252
MarioM <152995196+Ri0M@users.noreply.github.com>
2024-01-10 00:09:14 +0100

    Menu Interfaz Usuario - Fase 1
    El menu es capaz de leer los cantantes del usuario y los guarda correctamente en un archivo csv

--- AB_AP.cpp
+++ AB_AP.cpp
@@ -1,25 +1,69 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+void menu(){
+	cout << "Bienvenido, por favor seleccione una opción: " << endl;
+	cout << "1. Añadir cantante. " << endl;
+	cout << "2. Salir. " << endl;
+}
+
+void guardarCantante(const vector<string>& lista_nombres) {
+
+	ifstream empty_check("cantantes.csv");
+
+	bool archivoVacio = empty_check.peek() == ifstream::traits_type::eof();
+	empty_check.close();
+
+	ofstream archivo("cantantes.csv", ios::app);
+
+	if (archivo.is_open()) {
+
+		//Se comprueba si el archivo está vacío, en caso de estarlo, se añade el nombre de la columna.
+		if (archivoVacio) {
+			archivo << "cantante" << endl;
+		}
+
+		for (const string& nombre : lista_nombres) {
+			archivo << nombre << endl;
+		}
+		archivo.close();
+		cout << "Los nombres de los cantantes han sido registrados correctamente. " << endl;
+	}
+	else {
+		cout << "Error al cargar los nombres de los cantantes. " << endl;
+	}
+}
+
 int main() {
-	const string df = "C:\\Users\\USUARIO\\Desktop\\SpotifyData_ML.csv";
+	vector<string> lista_nombres;
 
-	ifstream archivo(df);
+	while (true) {
+		menu();
 
-	if (!archivo.is_open()) {
-		cerr << "No se pudo abrir el archivo" << endl;
-		return 1;
+		int opcion;
+		cin >> opcion;
+
+		switch (opcion) {
+		case 1: {
+			string nombre;
+			cout << "Ingrese el nombre del cantate: " << endl;
+			cin.ignore();
+			getline(cin, nombre);
+			lista_nombres.push_back(nombre);
+			break;
+		}
+		case 2: {
+			guardarCantante(lista_nombres);
+			return 0;
+		}
+		default:
+			cout << "Opcion no valida, introduzca una opcion valida. " << endl;
+		}
 	}
 
-	for (int i = 0; i < 5; i++) {
-		string linea;
-		getline(archivo, linea);
-		cout << linea << endl;
-	}
-
-	archivo.close();
 	return 0;
 }
